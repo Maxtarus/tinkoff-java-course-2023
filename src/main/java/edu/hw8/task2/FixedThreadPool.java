8269b432733644277a7d2397252e0b82d0273136
Maxtarus <m4xstarstarostin@yandex.ru>
2023-12-03 23:47:01 +0300

    fix checkstyle

--- src/main/java/edu/hw8/task2/FixedThreadPool.java
+++ src/main/java/edu/hw8/task2/FixedThreadPool.java
@@ -61,14 +61,4 @@ public class FixedThreadPool implements ThreadPool {
             }
         }
     }
-
-    public static void main(String[] args) {
-        final long[] fibonacciSequence = {
-            0L, 1L, 1L, 2L, 3L, 5L, 8L, 13L, 21L, 34L, 55L, 89L, 144L, 233L, 377L, 610L, 987L,
-            1597L, 2584L, 4181L, 6765L, 10946L, 17711L, 28657L, 46368L, 75025L, 121393L, 196418L,
-            317811L, 514229L, 832040L, 1346269L, 2178309L, 3524578L, 5702887L, 9227465L, 14930352L,
-
-        };
-        System.out.println(fibonacciSequence.length);
-    }
 }
